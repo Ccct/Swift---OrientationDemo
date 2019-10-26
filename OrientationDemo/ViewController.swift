c76ca127c80b16d1da32954e93379fe791ae298f
楚涛 <705356202@qq.com>
2019-10-26 15:21:28 +0800

    Initial Commit

---
+++ OrientationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrientationDemo
+//
+//  Created by mac on 2019/10/26.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
